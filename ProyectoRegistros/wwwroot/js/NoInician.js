d652a8b6370ece72b4b1f1f2f7c2c4158d6d4c06
Myriamm <PC3@DESKTOP-62NQLIF>
2025-09-05 15:23:03 -0600

    Index maso terminado y usuarios

---
+++ ProyectoRegistros/wwwroot/js/NoInician.js
@@ -0,0 +1,3 @@
+﻿document.getElementById("ig1").addEventListener("click", function () {
+    window.location.href = "../InicioSesion.html";
+});
---
+++ ProyectoRegistros/wwwroot/js/Profesor.js
@@ -0,0 +1,13 @@
+﻿document.getElementById('ig2').addEventListener('click', function (event) {
+    event.stopPropagation();
+    var historial = document.querySelector('.historial');
+    historial.classList.toggle('historial-activo');
+});
+
+document.addEventListener('click', function (event) {
+    var historial = document.querySelector('.historial');
+    if (historial.classList.contains('historial-activo') && !historial.contains(event.target) && event.target.id !== 'ig2') {
+        historial.classList.remove('historial-activo');
+    }
+});
+
