57546d0a1c1f0aa59ab0fbfe77597e5be6ae716b
Myriamm <PC3@DESKTOP-62NQLIF>
2025-09-10 15:21:04 -0600

    No sé

--- ProyectoRegistros/wwwroot/js/Profesor.js
+++ ProyectoRegistros/wwwroot/js/Profesor.js
@@ -1,4 +1,15 @@
-﻿document.getElementById('ig2').addEventListener('click', function (event) {
+﻿let aside = document.getElementById("menu-abierto");
+
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("menu")) { // busca por clase
+        aside.classList.toggle("visible");
+    }
+    else {
+        aside.classList.remove("visible");
+    }
+})
+
+document.getElementById('ig2').addEventListener('click', function (event) {
     event.stopPropagation();
     var historial = document.querySelector('.historial');
     historial.classList.toggle('historial-activo');
